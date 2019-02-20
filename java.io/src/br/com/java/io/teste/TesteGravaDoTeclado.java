a50606786d5c0b784247ceb701b56a1295b80105
diegosouzaba <diego.souzaba@hotmail.com>
2019-02-20 11:23:22 -0300

    + classes

---
+++ java.io/src/br/com/java/io/teste/TesteGravaDoTeclado.java
@@ -0,0 +1,42 @@
+package br.com.java.io.teste;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+
+
+public class TesteGravaDoTeclado {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		InputStream fis = System.in;
+		Reader isr = new InputStreamReader(fis);
+		BufferedReader br = new BufferedReader(isr);
+		String linha = br.readLine();
+		
+		OutputStream fos = new FileOutputStream("lorem2.txt");
+		Writer osw = new OutputStreamWriter(fos);
+		BufferedWriter bw = new BufferedWriter(osw);
+		
+		while(linha != null && !linha.isEmpty()){
+			bw.write(linha);
+			bw.newLine();
+			linha = br.readLine();
+			
+		}
+		
+		bw.close();
+		br.close();
+
+	}
+
+}
